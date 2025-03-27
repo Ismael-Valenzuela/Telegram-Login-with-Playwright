8bbc137140af200f2efa29e7d4a742b4d6bd60ee
Ismael <ismael.vm@strivelabs.io>
2025-03-27 14:35:42 +0100

    SECURITY

--- README.md
+++ README.md
@@ -3,14 +3,30 @@
 - Clone this repository from **Github**.
 - Open the folder in you favourite **IDE** (like **Visual Studio Code**) and open the **CMD** to execute the next commands.
 - Install the next commands so you can use playwright in your machine:
+
   ```bash
   npm install playwright
   ```
+
+  ```bash
+  npm install dotenv
+  ```
+
   ```bash
   npx install playwright
   ```
+
   **(⚠️ MAKE SURE TO HAVE NODE INSTALLED: https://nodejs.org/es ⚠️)**
-- First, we need to log in Telegram manually to save the cookies in your local folder. The command you have to execute is:
+
+- Once we installed everything, we have to create a folder named **.env**
+- Inside **.env** we are going to create the next enviroment variable:
+
+  ```bash
+    SECRET_KEY="Insert_here_a_key_with_32_characters"
+  ```
+
+- Now, all the session files will be encrypted and secure in your local machine. To decript one of those files you will neex the SECRET_KEY that you put when you created the session, that is why is important to create the enviroment variable.
+- We need to log in Telegram manually to save the cookies in your local folder. The command you have to execute is:
 
   ```bash
   npx playwright test tests/login.spec.ts
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,33 @@
+require("dotenv").config();
 import { test, devices, Page, BrowserContext } from "@playwright/test";
+import * as crypto from "crypto";
+import * as fs from "fs";
 
+const myKey: any = process.env.SECRET_KEY; // Here we are using the secret key from the .env file that MUST BE 32 CHARACTERS
+const SECRET_KEY = crypto.createHash("sha256").update(myKey).digest();
+const ALGORITHM = "aes-256-cbc";
+const IV = crypto.randomBytes(16);
+
+// Function to encrypt a file
+function encryptFile(inputPath: string, outputPath: string) {
+  const data = fs.readFileSync(inputPath, "utf8");
+  const cipher = crypto.createCipheriv(ALGORITHM, SECRET_KEY, IV);
+  let encrypted = cipher.update(data, "utf8", "hex");
+  encrypted += cipher.final("hex");
+
+  const encryptedData = {
+    iv: IV.toString("hex"),
+    content: encrypted,
+  };
+
+  fs.writeFileSync(outputPath, JSON.stringify(encryptedData, null, 2));
+  console.log(`Archivo encriptado guardado en: ${outputPath}`);
+}
+
+// This is the test of login
+const timeToLogin : number = 60000; // This is the max time you have to login manually, IF YOU NEED MORE TIME, CHANGE THIS VALUE TO A BIGGER ONE
 test("Save session of TG", async ({ browser }) => {
-  test.setTimeout(70000); // This is the max time the test have to be. (DO NOT CLOSE THE BROWSER UNTIL THE TEST FINISHES, NOT EVEN IF YOU ALREADY LOGGED IN)
+  test.setTimeout(timeToLogin);
   const context: BrowserContext = await browser.newContext({
     ...devices["iPhone 12"],
   });
@@ -9,10 +35,18 @@ test("Save session of TG", async ({ browser }) => {
 
   await page.goto("https://web.telegram.org/");
   console.log("Please, log in Telegram manually. (You have 60 seconds)");
-  await page.waitForTimeout(60000); // This give you 60 seconds to log in, if you can't log in in that time, change this value AND the test.setTimeout() at the beggining with a margin of 5-10 seconds.
+  await page.waitForTimeout(timeToLogin);
 
-  await context.storageState({ path: "telegram-session.json" }); //This is the magic line that saves the session into the storageState of the browser.
-  console.log("Session saved in: telegram-session.json");
+  // Save the session
+  const sessionPath = "crypto/telegram-session.json";
+  await context.storageState({ path: sessionPath }); // Here is where the session is saved inside the storage State of the Browser so we can use it later
+
+  // Encrypt the session
+  const encryptedPath = "crypto/telegram-session.enc.json";
+  encryptFile(sessionPath, encryptedPath);
+
+  // Delete the original session
+  fs.unlinkSync(sessionPath);
 
   await browser.close();
 });
